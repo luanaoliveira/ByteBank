bf13479f9ff7cd53fe49ef586559bba96ddcd88c
Luana Oliveira <luanasilva.jua@gmail.com>
2023-01-04 03:14:29 -0300

    creating customer list

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -24,6 +24,8 @@ namespace ByteBank
             Console.WriteLine("5 - Sacar da conta de um cliente");
             Console.WriteLine("6 - Transferir entre contas");
             Console.WriteLine("0 - Sair do programa");
+            Console.WriteLine();
+            Console.Write("Opção: ");
             return int.Parse(Console.ReadLine());
         }
 
@@ -40,21 +42,42 @@ namespace ByteBank
             newClient.balance = 0;
             return newClient;
         }
+
+        static void ListClients (Client[] clients)
+        {
+            Console.WriteLine("Lista de clientes");
+            Console.WriteLine();
+            for (int i = 0; i < clients.Length; i++)
+            {
+                Console.WriteLine($"Index {i}");
+                Console.WriteLine($"Nome: {clients[i].name}");
+                Console.WriteLine($"CPF: {clients[i].cpf}");
+                Console.WriteLine("----");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
         public static void Main(string[] args)
         {
-            Client[] clients = new Client[10];
+            Client[] clients = new Client[0];
             int option;
 
             do
             {
+                Console.Clear();
                 Program.Header();
                 option = Program.MainMenu();
 
                 switch(option)
                 {
-                    case 1: 
-                        Client c = InputClient();
-                        clients.Append(c);
+                    case 1:
+                        Console.Clear();
+                        Client newClient = InputClient();
+                        clients = clients.Append(newClient).ToArray();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        ListClients(clients);
                         break;
                 }
                 
